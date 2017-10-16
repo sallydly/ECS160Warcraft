fd3919f4a75b47128765192acf8aded529729d44
Kevin Ow <k3vin15243@gmail.com>
2017-10-16 00:08:01 -0700

    Refactor AJ's code and update .gitignore

--- app/src/main/java/com/example/gadau/warcraftmock/Options.java
+++ app/src/main/java/com/example/gadau/warcraftmock/Options.java
@@ -1,11 +1,9 @@
 package com.example.gadau.warcraftmock;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class Options extends AppCompatActivity {
 
@@ -16,23 +14,25 @@ public class Options extends AppCompatActivity {
 
     }
 
-
-    /*
+    /**
      * Overridden to display MotionEvent values
      * e.g. x coordinate from motionEvent.getX()
+     *
+     * @param motionEvent : Event to process
+     * @return : Returns true if you have consumed the event, false if you haven't.
      */
     @Override
     public boolean onTouchEvent(MotionEvent motionEvent) {
-        float deltaX, deltaY;
+        double deltaX, deltaY;
 
-        TextView textView = (TextView) findViewById(R.id.optionsTextView);
-        TextView xCoordDisplay = (TextView) findViewById(R.id.xCoordOptions);
-        TextView yCoordDisplay = (TextView) findViewById(R.id.yCoordOptions);
+        TextView textView = (TextView)findViewById(R.id.optionsTextView);
+        TextView xCoordDisplay = (TextView)findViewById(R.id.xCoordOptions);
+        TextView yCoordDisplay = (TextView)findViewById(R.id.yCoordOptions);
 
-        textView.setText(motionEvent.actionToString(motionEvent.getActionMasked()));
-        xCoordDisplay.setText(Float.toString(motionEvent.getX()));
-        yCoordDisplay.setText(Float.toString(motionEvent.getY()));
+        textView.setText(MotionEvent.actionToString(motionEvent.getActionMasked()));
+        xCoordDisplay.setText(String.valueOf(motionEvent.getX()));
+        yCoordDisplay.setText(String.valueOf(motionEvent.getY()));
+
         return super.onTouchEvent(motionEvent);
-
     }
 }
