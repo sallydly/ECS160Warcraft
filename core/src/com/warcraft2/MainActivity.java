93fe67760732b636ce7dce58325fe553f4421b63
Hung Mai <hqmai@ucdavis.edu>
2017-10-20 16:19:46 -0700

    Complete displaying splash (not perfectly) screen and splash screen transition to main menu screen.

--- core/src/com/warcraft2/MainActivity.java
+++ core/src/com/warcraft2/MainActivity.java
@@ -1,33 +1,38 @@
 package com.warcraft2;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MainActivity extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+//  Game class also implements ApplicationListener
+public class MainActivity extends Game {
+	//SpriteBatch batch;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        setScreen(new Splash(this));
+		//batch = new SpriteBatch();
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+        super.render();
+		//Gdx.gl.glClearColor(1, 1, 1, 1);
+		//Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		//batch.begin();
+
+		//batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+        super.render();
+		//batch.dispose();
+
 	}
 }
---
+++ core/src/com/warcraft2/Splash.java
@@ -0,0 +1,89 @@
+package com.warcraft2;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.TimeUtils;
+
+/**
+ * Created by hqmai on 10/20/17.
+ */
+
+public class Splash implements Screen {
+
+    private Game game;
+    private SpriteBatch batch;
+    private Sprite splash;
+    private Texture texture;
+    private float duration;
+    private float currentDuration;
+
+    public Splash(Game g) {
+        game = g;
+    }
+
+    @Override
+    public void show() {
+        batch = new SpriteBatch();
+        texture = new Texture("img/Splash.png");
+        splash = new Sprite(texture);
+        //  get time time when splash starts
+        currentDuration = 0;
+        //  set duration of splash screen
+        duration = 2;
+        /*
+         *  Splash.png contain 2 images, so Gdx.graphics.getHeight()*2 is use to set the upper
+         *  image (to set to lower image, use setPosition(0, 0) instead) to be the splash screen,
+         *  ultimately we want to parse one of the image.
+         *  TODO: find a solution to correctly parse Splash.png image
+         */
+        splash.setPosition(0, -Gdx.graphics.getHeight());
+        splash.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()*2);
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        //  make splash draw itself onto the screen
+        splash.draw(batch);
+        batch.end();
+        currentDuration = currentDuration + delta;
+        if(currentDuration > duration) {
+            game.setScreen(new MainMenu());
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        texture.dispose();
+        game.dispose();
+    }
+}
