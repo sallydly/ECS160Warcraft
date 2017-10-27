85b7617aab79854f82721e4d8907b83a8f5b5159
kctanaka <kctanaka@ucdavis.edu>
2017-10-27 15:45:30 -0700

    Added GameDataTypes and  Initialization structs

---
+++ core/src/com/warcraftII/asset/SAssetInitialization.java
@@ -0,0 +1,19 @@
+package com.warcraftII.asset;
+
+import com.warcraftII.GameDataTypes.*;
+
+/**
+ *
+ * This was formerly a struct in CAssetDecoratedMap,
+ * changed to class because Java doesn't have structs
+ *
+ * Created by Kimi on 10/27/2017.
+ */
+
+public class SAssetInitialization {
+    String DType;
+    EPlayerColor DColor;
+    // TODO: Uncomment when AJ finishes porting Position
+    // CTilePosition DTilePosition;
+
+}
